eecec101428b3445257f26c256de92fde14d004c
Dwayne Forsyth <Dwayne@2dkits.com>
2019-04-30 08:41:17 -0500

    update code, validated hardware /w 1 fix

--- main/LED_Driver.c
+++ main/LED_Driver.c
@@ -1,11 +1,6 @@
-/* SPI Master example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,7 +12,7 @@
 #include "driver/gpio.h"
 
 /*
- This code drives a chain of ata102 smart LED chains.
+   This code drive the 2DKits.com 4x4x8 tower
 */
 
 #define BLINK_GPIO   02
@@ -27,14 +22,14 @@
 #define PIN_NUM_CS   04
 
 #define STROBE       18
-#define COMSIG0      03
-#define COMSIG1      22
-#define COMSIG2      23
-#define COMSIG3      19
-#define COMSIG4      21
-#define COMSIG5      05
-#define COMSIG6      17
-#define COMSIG7      16
+#define COMSIG0      15 // 15
+#define COMSIG1      23 // 22
+#define COMSIG2      22 // 23
+#define COMSIG3      19 // 19
+#define COMSIG4      17 // 21
+#define COMSIG5      21 // 05
+#define COMSIG6      05 // 17
+#define COMSIG7      16 // 16
 
 
 #define NUMBER_OF_LEDS 6
@@ -50,7 +45,7 @@ static uint8_t strobe = 0;
 static uint8_t oldStrobe = 0;
 bool spi_done = true;
 
-static const uint8_t strobeGPIO[8] = {
+static const uint8_t strobeGPIO[9] = {
         COMSIG0,
         COMSIG1,
         COMSIG2,
@@ -58,7 +53,8 @@ static const uint8_t strobeGPIO[8] = {
         COMSIG4,
         COMSIG5,
         COMSIG6,
-        COMSIG7
+        COMSIG7,
+	0
     };
 
 void updateMBI5026Chain(spi_device_handle_t spi, uint16_t red, uint16_t green, uint16_t blue) {
@@ -82,16 +78,12 @@ void updateMBI5026Chain(spi_device_handle_t spi, uint16_t red, uint16_t green, u
     spi_device_queue_trans(spi, &t, portMAX_DELAY);  //Transmit!
 }
 
-void spi_pre_transfer_callback() {
+void IRAM_ATTR spi_pre_transfer_callback() {
     gpio_set_level(PIN_NUM_CS, 0);
     spi_done = false;
-    gpio_set_level(strobeGPIO[oldStrobe], 0);
 }
 
-void spi_post_transfer_callback() {
-    gpio_set_level(strobeGPIO[oldStrobe], 0);
-    gpio_set_level(strobeGPIO[oldStrobe], 0);
-    gpio_set_level(strobeGPIO[oldStrobe], 0);
+void IRAM_ATTR spi_post_transfer_callback() {
     gpio_set_level(strobeGPIO[oldStrobe], 0);
     gpio_set_level(STROBE, 1);
     gpio_set_level(STROBE, 0);
@@ -102,7 +94,7 @@ void spi_post_transfer_callback() {
 }
 
 
-uint16_t ledDataOut[8][16][4];
+uint16_t ledDataOut[9][16][4];
 
 void setLed(uint8_t z, uint8_t x, uint8_t y, uint8_t iR, uint8_t iG, uint8_t iB) {
     uint8_t oStrobe = 0;
@@ -112,7 +104,6 @@ void setLed(uint8_t z, uint8_t x, uint8_t y, uint8_t iR, uint8_t iG, uint8_t iB)
 
     switch(z) {
 	case 0:
-        default:
             oStrobe = x;
             oOffset = 1<<y;
 	    break;
@@ -120,13 +111,32 @@ void setLed(uint8_t z, uint8_t x, uint8_t y, uint8_t iR, uint8_t iG, uint8_t iB)
             oStrobe = x+4;
             oOffset = 1<<(y);
 	    break;
-	case 2:
+	case 4:
             oStrobe = x;
             oOffset = 1<<(y+4);
 	    break;
-	case 3:
+	case 5:
             oStrobe = x+4;
             oOffset = 1<<(y+4);
+	    break;
+	case 6:
+            oStrobe = x;
+            oOffset = 1<<(y+8);
+	    break;
+	case 7:
+            oStrobe = x+4;
+            oOffset = 1<<(y+8);
+	    break;
+	case 2:
+            oStrobe = x;
+            oOffset = 1<<(y+12);
+	    break;
+	case 3:
+            oStrobe = x+4;
+            oOffset = 1<<(y+12);
+	    break;
+        default:
+            printf("out of range layer = %d\n",z);
 	    break;
     }
 
@@ -141,16 +151,20 @@ void setLed(uint8_t z, uint8_t x, uint8_t y, uint8_t iR, uint8_t iG, uint8_t iB)
 	} else {
 	    ledDataOut[oStrobe][ti][1] &= ~oOffset;
 	}
-    #if (1)
 	if (ti < iB) {
 	    ledDataOut[oStrobe][ti][2] |= oOffset;
 	} else {
 	    ledDataOut[oStrobe][ti][2] &= ~oOffset;
 	}
-#endif
     }
 };
 
+//
+// this is the highest pri task, and it is currently running full speed
+// it releases controll 1/9 of time while all the leds are off.
+//
+// have a bug where strobe 0 is running brigher then the others
+//
 void updateLedTask(void *param) {
     static uint8_t intensity = 0;
 
@@ -160,9 +174,10 @@ void updateLedTask(void *param) {
             intensity = 0;
 	    oldStrobe = strobe;
             strobe++;
-	    if (strobe >= 7) {
+	    if (strobe >= 8) {
 		 strobe = 0;
-	         vTaskDelay(1);
+                 gpio_set_level(strobeGPIO[7], 0);
+	         vTaskDelay(1); // all leds are off, release control
 	    }
         }
 
@@ -174,17 +189,37 @@ void updateLedTask(void *param) {
     }
 }
 
+//
+// bypass all the math and sets the strob data to all zeros.
+//
 void allLedsOff() {
     uint8_t s,i,c;
-    for (s=0;s<8;s++) {
+    for (s=0;s<9;s++) {
         for (i=0;i<16;i++) {
             for (c=0;c<3;c++) {
                 ledDataOut[s][i][c] = 0x0000;
 }   }   }   } 
 
+void allLedsOn() {
+    uint8_t s,i,c;
+    for (s=0;s<9;s++) {
+        for (i=0;i<16;i++) {
+            for (c=0;c<3;c++) {
+                ledDataOut[s][i][c] = 0xffff;
+}   }   }   } 
 
-void app_main()
-{
+//
+//
+//
+void allLedsColor( uint8_t red, uint8_t green, uint8_t blue) {
+    uint8_t l,x,y;
+    for (l=0;l<8;l++) {
+        for (x=0;x<4;x++) {
+            for (y=0;y<4;y++) {
+               setLed(l,x,y, red,green,blue);
+}   }   }   }
+
+void init_LED_driver() {
     esp_err_t ret;
     uint8_t i;
     const spi_bus_config_t buscfg={
@@ -207,8 +242,6 @@ void app_main()
 	.post_cb=spi_post_transfer_callback,
     };
 
-    printf("Hello from Tower\n");
-
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_level(BLINK_GPIO, 0);
@@ -221,7 +254,7 @@ void app_main()
     gpio_set_direction(STROBE, GPIO_MODE_OUTPUT);
     gpio_set_level(STROBE, 0);
 
-    for (i=0;i<7l;i++) {
+    for (i=0;i<8;i++) {
         gpio_pad_select_gpio(strobeGPIO[i]);
         gpio_set_direction(strobeGPIO[i], GPIO_MODE_OUTPUT);
         gpio_set_level(strobeGPIO[i], 0);
@@ -235,72 +268,6 @@ void app_main()
     ESP_ERROR_CHECK(ret);
 
     allLedsOff();
-    setLed(0,0,0, 15,0,15);
-    setLed(0,1,1, 8,0,0);
-    setLed(0,2,2, 4,0,0);
-    setLed(0,3,3, 1,0,0);
-    setLed(0,3,3, 0,0,0);
- 
-    for (i=0;i<7;i++) {
-        printf("i=%d R=%04X\n",i, ledDataOut[3][i][0]);
-    }
 
     xTaskCreate(updateLedTask, "updateLedTask", 4*1024, NULL, 24, NULL);
-    printf("about to loop\n");
-
-    bool dr = true;
-    bool db = true;
-    bool dg = false;
-    uint8_t j = 0;
-    uint8_t r = 0;
-    uint8_t b = 11;
-    uint8_t g = 11;
-
-    while(1) {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-	if (db==true) {
-	   b++;
-	} else {
-	   b--;
-	}
-
-	if (b==15) {
-           db = false;
-	} else if (b==1) {
-	   db = true;
-	}
-
-	if (dr==true) {
-	   r++;
-	} else {
-	   r--;
-	}
-
-	if (r==15) {
-           dr = false;
-	} else if (r==1) {
-	   dr = true;
-	}
-
-	if (dg==true) {
-	   g++;
-	} else {
-	   g--;
-	}
-
-	if (g==15) {
-           dg = false;
-	} else if (g==1) {
-	   dg = true;
-	}
-
-        setLed(0,0,0, r,g,b);
-        setLed(0,0,3, r,0,b);
-        setLed(0,3,3, 0,g,b);
-        setLed(0,3,0, r,g,0);
-        setLed(0,2,1, g,g,g);
-        setLed(0,1,2, r,r,r);
-
-        printf("r=%d g=%d b=%d\n",r,g,b);
-    }
 }
---
+++ main/LED_Driver.h
@@ -0,0 +1,9 @@
+
+void init_LED_driver();
+void setLed(uint8_t z, uint8_t x, uint8_t y, uint8_t iR, uint8_t iG, uint8_t iB);
+
+void allLedsOff();
+void allLedsOn();
+void allLedsColor( uint8_t red, uint8_t green, uint8_t blue);
+
+
