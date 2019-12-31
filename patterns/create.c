f564fe48a41ec17292784d661f41ab6ae95ba4e3
Dwayne Forsyth <DwayneF@OptimalDesignCo.com>
2019-12-31 11:24:17 -0600

    updated pattern boom

--- patterns/create.c
+++ patterns/create.c
@@ -65,6 +65,7 @@ void allLedsOff(void) {
 
 const char *LEDValue[19] = { "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","+","-","#" };
 
+static uint16_t frame=0;
 void printHeader(void) {
    printf("#######################################\n");
    printf("name=%s\n",header.name);
@@ -72,11 +73,11 @@ void printHeader(void) {
    printf("speed=%d\n",header.speed);
    printf("cycles=%d\n",header.cycles);
    printf("#######################################\n");
+   frame = 0;
 }
 void printFrame(void) {
    int8_t l,x,y;
    uint8_t red,green,blue;
-   static uint16_t frame=0;
 
    printf("\nframe %d\n",frame);
    for (l=7;l>=0;l--) {
@@ -98,224 +99,231 @@ void writeFrame(FILE *ptr) {
 }
 
 void make_boom(void) {
-   int8_t l,x,y;
+    int8_t l,x,y;
 
-        FILE *ptr = fopen("boom.pat","wb");
-        strncpy(header.name,"Boom",16);
-        fwrite( &header, sizeof(header), 1, ptr);
-        printHeader();
+    FILE *ptr = fopen("boom.pat","wb");
+    strncpy(header.name,"Boom",16);
+    fwrite( &header, sizeof(header), 1, ptr);
+    printHeader();
+    uint8_t color, red_up, green_up, blue_up;
 
-	allLedsColor(LED_MINUS,LED_NOOP,LED_NOOP);
+    for (color=1;color < 8;color++) {
+        red_up     = (color & 0x01)? LED_PLUS  : LED_NOOP; 
+        green_up   = (color & 0x02)? LED_PLUS  : LED_NOOP; 
+        blue_up    = (color & 0x04)? LED_PLUS  : LED_NOOP; 
+
+        allLedsColor(LED_MINUS, LED_MINUS, LED_MINUS);
         for (l=3;l<5;l++) {
-	    setLed(l,1,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, red_up, green_up, blue_up);
+            setLed(l,1,2, red_up, green_up, blue_up);
+            setLed(l,2,1, red_up, green_up, blue_up);
+            setLed(l,2,2, red_up, green_up, blue_up);
         }
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         for (l=2;l<6;l++) {
-	    setLed(l,1,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, red_up, green_up, blue_up);
+            setLed(l,1,2, red_up, green_up, blue_up);
+            setLed(l,2,1, red_up, green_up, blue_up);
+            setLed(l,2,2, red_up, green_up, blue_up);
         }
         for (l=3;l<5;l++) {
-	    setLed(l,0,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,0,1, red_up, green_up, blue_up);
+            setLed(l,0,2, red_up, green_up, blue_up);
+            setLed(l,1,0, red_up, green_up, blue_up);
+            setLed(l,1,3, red_up, green_up, blue_up);
+            setLed(l,2,0, red_up, green_up, blue_up);
+            setLed(l,2,3, red_up, green_up, blue_up);
+            setLed(l,3,1, red_up, green_up, blue_up);
+            setLed(l,3,2, red_up, green_up, blue_up);
         }
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
-
+    
         for (l=1;l<7;l++) {
-	    setLed(l,1,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, red_up, green_up, blue_up);
+            setLed(l,1,2, red_up, green_up, blue_up);
+            setLed(l,2,1, red_up, green_up, blue_up);
+            setLed(l,2,2, red_up, green_up, blue_up);
         }
         for (l=2;l<6;l++) {
-	    setLed(l,0,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,0,1, red_up, green_up, blue_up);
+            setLed(l,0,2, red_up, green_up, blue_up);
+            setLed(l,1,0, red_up, green_up, blue_up);
+            setLed(l,1,3, red_up, green_up, blue_up);
+            setLed(l,2,0, red_up, green_up, blue_up);
+            setLed(l,2,3, red_up, green_up, blue_up);
+            setLed(l,3,1, red_up, green_up, blue_up);
+            setLed(l,3,2, red_up, green_up, blue_up);
         }
-        levelLedsColor(3, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_PLUS, LED_NOOP, LED_NOOP);
+        levelLedsColor(3, red_up, green_up, blue_up);
+        levelLedsColor(4, red_up, green_up, blue_up);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         for (l=0;l<8;l++) {
-	    setLed(l,1,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, red_up, green_up, blue_up);
+            setLed(l,1,2, red_up, green_up, blue_up);
+            setLed(l,2,1, red_up, green_up, blue_up);
+            setLed(l,2,2, red_up, green_up, blue_up);
         }
         for (l=1;l<7;l++) {
-	    setLed(l,0,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,0,1, red_up, green_up, blue_up);
+            setLed(l,0,2, red_up, green_up, blue_up);
+            setLed(l,1,0, red_up, green_up, blue_up);
+            setLed(l,1,3, red_up, green_up, blue_up);
+            setLed(l,2,0, red_up, green_up, blue_up);
+            setLed(l,2,3, red_up, green_up, blue_up);
+            setLed(l,3,1, red_up, green_up, blue_up);
+            setLed(l,3,2, red_up, green_up, blue_up);
         }
-        levelLedsColor(2, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(3, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(5, LED_PLUS, LED_NOOP, LED_NOOP);
+        levelLedsColor(2, red_up, green_up, blue_up);
+        levelLedsColor(3, red_up, green_up, blue_up);
+        levelLedsColor(4, red_up, green_up, blue_up);
+        levelLedsColor(5, red_up, green_up, blue_up);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         for (l=0;l<8;l++) {
-	    setLed(l,0,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_PLUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_PLUS, LED_NOOP, LED_NOOP);
+            setLed(l,0,1, red_up, green_up, blue_up);
+            setLed(l,0,2, red_up, green_up, blue_up);
+            setLed(l,1,0, red_up, green_up, blue_up);
+            setLed(l,1,3, red_up, green_up, blue_up);
+            setLed(l,2,0, red_up, green_up, blue_up);
+            setLed(l,2,3, red_up, green_up, blue_up);
+            setLed(l,3,1, red_up, green_up, blue_up);
+            setLed(l,3,2, red_up, green_up, blue_up);
         }
-        levelLedsColor(1, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(2, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(3, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(5, LED_PLUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(6, LED_PLUS, LED_NOOP, LED_NOOP);
+        levelLedsColor(1, red_up, green_up, blue_up);
+        levelLedsColor(2, red_up, green_up, blue_up);
+        levelLedsColor(3, red_up, green_up, blue_up);
+        levelLedsColor(4, red_up, green_up, blue_up);
+        levelLedsColor(5, red_up, green_up, blue_up);
+        levelLedsColor(6, red_up, green_up, blue_up);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
-	allLedsColor(LED_PLUS,LED_NOOP,LED_NOOP);
+    allLedsColor( red_up, green_up, blue_up);
         for (l=3;l<5;l++) {
-	    setLed(l,1,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_MINUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,1,2, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,2, LED_MINUS, LED_MINUS, LED_MINUS);
         }
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+    patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         for (l=2;l<6;l++) {
-	    setLed(l,1,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_MINUS, LED_NOOP, LED_NOOP);
+            setLed(l,1,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,1,2, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,2, LED_MINUS, LED_MINUS, LED_MINUS);
+         }
+         for (l=3;l<5;l++) {
+             setLed(l,0,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,0,2, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,0, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,3, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,0, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,3, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,3,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,3,2, LED_MINUS, LED_MINUS, LED_MINUS);
+         }
+         patternFrame.cycles = 8;
+         patternFrame.delay = 1;
+         writeFrame(ptr);
+         printFrame();
+ 
+         for (l=1;l<7;l++) {
+             setLed(l,1,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,2, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,2, LED_MINUS, LED_MINUS, LED_MINUS);
+         }
+         for (l=2;l<6;l++) {
+             setLed(l,0,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,0,2, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,0, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,3, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,0, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,3, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,3,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,3,2, LED_MINUS, LED_MINUS, LED_MINUS);
+         }
+         levelLedsColor(3, LED_MINUS, LED_MINUS, LED_MINUS);
+         levelLedsColor(4, LED_MINUS, LED_MINUS, LED_MINUS);
+         patternFrame.cycles = 8;
+         patternFrame.delay = 1;
+         writeFrame(ptr);
+         printFrame();
+ 
+         for (l=0;l<8;l++) {
+             setLed(l,1,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,2, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,2,2, LED_MINUS, LED_MINUS, LED_MINUS);
+         }
+         for (l=1;l<7;l++) {
+             setLed(l,0,1, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,0,2, LED_MINUS, LED_MINUS, LED_MINUS);
+             setLed(l,1,0, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,1,3, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,0, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,3, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,3,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,3,2, LED_MINUS, LED_MINUS, LED_MINUS);
         }
-        for (l=3;l<5;l++) {
-	    setLed(l,0,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_MINUS, LED_NOOP, LED_NOOP);
-        }
+        levelLedsColor(2, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(3, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(4, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(5, LED_MINUS, LED_MINUS, LED_MINUS);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
-        writeFrame(ptr);
-        printFrame();
-
-        for (l=1;l<7;l++) {
-	    setLed(l,1,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_MINUS, LED_NOOP, LED_NOOP);
-        }
-        for (l=2;l<6;l++) {
-	    setLed(l,0,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_MINUS, LED_NOOP, LED_NOOP);
-        }
-        levelLedsColor(3, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_MINUS, LED_NOOP, LED_NOOP);
-        patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         for (l=0;l<8;l++) {
-	    setLed(l,1,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,2, LED_MINUS, LED_NOOP, LED_NOOP);
+            setLed(l,0,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,0,2, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,1,0, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,1,3, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,0, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,2,3, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,3,1, LED_MINUS, LED_MINUS, LED_MINUS);
+            setLed(l,3,2, LED_MINUS, LED_MINUS, LED_MINUS);
         }
-        for (l=1;l<7;l++) {
-	    setLed(l,0,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_MINUS, LED_NOOP, LED_NOOP);
-        }
-        levelLedsColor(2, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(3, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(5, LED_MINUS, LED_NOOP, LED_NOOP);
+        levelLedsColor(1, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(2, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(3, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(4, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(5, LED_MINUS, LED_MINUS, LED_MINUS);
+        levelLedsColor(6, LED_MINUS, LED_MINUS, LED_MINUS);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
-        for (l=0;l<8;l++) {
-	    setLed(l,0,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,0,2, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,1,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,0, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,2,3, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,1, LED_MINUS, LED_NOOP, LED_NOOP);
-	    setLed(l,3,2, LED_MINUS, LED_NOOP, LED_NOOP);
-        }
-        levelLedsColor(1, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(2, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(3, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(4, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(5, LED_MINUS, LED_NOOP, LED_NOOP);
-        levelLedsColor(6, LED_MINUS, LED_NOOP, LED_NOOP);
+        allLedsColor(LED_MINUS, LED_MINUS, LED_MINUS);
         patternFrame.cycles = 8;
-	patternFrame.delay = 1;
-        writeFrame(ptr);
-        printFrame();
-
-	allLedsColor(LED_MINUS,LED_NOOP,LED_NOOP);
-        patternFrame.cycles = 8;
-	patternFrame.delay = 1;
+        patternFrame.delay = 1;
         writeFrame(ptr);
         printFrame();
 
         fclose(ptr);
+    }
 }
 void make_test(void) {
    int8_t l,x,y;
