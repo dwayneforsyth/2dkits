5586e834d311892b290e2d898da89afa602ab6d5
Dwayne Forsyth <Dwayne@2dkits.com>
2020-09-19 17:33:45 -0500

    move to 0.0.2

--- main/version.h
+++ main/version.h
@@ -4,7 +4,7 @@
 
 #define MAJOR 0
 #define MINOR 0
-#define BUILD 1
+#define BUILD 2
 
 #define CONFIG_FIRMWARE_STATUS_URL      "http://www.2dkits.com/kits/kit25/current_firmware.txt"
 #define CONFIG_TEST_FIRMWARE_STATUS_URL "http://www.2dkits.com/kits/kit25/current_test_firmware.txt"
