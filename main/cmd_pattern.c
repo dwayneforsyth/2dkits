c632ad4f034f9d6b0146ba90c44cb41a32b495c2
Dwayne Forsyth <Dwayne@2dkits.com>
2021-08-17 23:36:05 -0500

    cleanup and bug fix

--- main/cmd_pattern.c
+++ main/cmd_pattern.c
@@ -19,6 +19,7 @@
 #include "version.h"
 #include "disk_system.h"
 #include "pattern_engine.h"
+#include "led_driver.h"
 
 #define WITH_TASKS_INFO 1
 
@@ -26,6 +27,7 @@ static const char *TAG = "cmd_system";
 
 static void register_pattern(void);
 static void register_demo(void);
+static void register_led(void);
 
 /*******************************************************************************
 
@@ -41,6 +43,7 @@ void commands_pattern(void)
 {
     register_pattern();
     register_demo();
+    register_led();
 }
 
 /*******************************************************************************
@@ -173,5 +176,106 @@ static void register_demo(void)
     ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
     demo_args.control = arg_str0(NULL, NULL, "<on|off>", "Get info of Ethernet");
     demo_args.end = arg_end(1);
+}
+
+/*******************************************************************************
+
+    PURPOSE: led command
+
+    INPUTS: None
+
+    RETURN CODE: None
+
+*******************************************************************************/
+static struct {
+    struct arg_lit *set;
+    struct arg_int *layer;
+    struct arg_int *x;
+    struct arg_int *y;
+    struct arg_int *red;
+    struct arg_int *green;
+    struct arg_int *blue;
+    struct arg_end *end;
+} led_args;
+
+static int command_led(int argc, char **argv)
+{
+    uint8_t layer = 0;
+    uint8_t x = 0;
+    uint8_t y = 0;
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+
+    int nerrors = arg_parse(argc, argv, (void **)&led_args);
+    if (nerrors != 0) {
+        arg_print_errors(stderr, led_args.end, argv[0]);
+        return 1;
+    }
+
+    if (led_args.layer->count) {
+        layer = led_args.layer->ival[0];
+	if (layer > 7) { return(1);}
+    }
+    if (led_args.x->count) {
+        x = led_args.x->ival[0];
+	if (x > 3) { return(1);}
+    }
+    if (led_args.y->count) {
+        y = led_args.y->ival[0];
+	if (y > 3) { return(1);}
+    }
+    if (led_args.red->count) {
+        r = led_args.red->ival[0];
+	if (r > 15) { return(1);}
+    }
+    if (led_args.green->count) {
+        g = led_args.green->ival[0];
+	if (g > 15) { return(1);}
+    }
+    if (led_args.blue->count) {
+        b = led_args.blue->ival[0];
+	if (b > 15) { return(1);}
+    }
+
+    if (led_args.set->count) {
+        ESP_LOGW(TAG, "set led Layer=%d x=%d y=%d -> (%d,%d,%d)",layer,x,y,r,g,b);
+        setLed(layer,x,y,r,g,b);
+    }
+
+    getLed(layer,x,y,&r,&g,&b);
+    ESP_LOGW(TAG, "get led Layer=%d x=%d y=%d -> (%d,%d,%d)",layer,x,y,r,g,b);
+
+
+    return 0;
+}
+
+/*******************************************************************************
+
+    PURPOSE: register 'next' command
+
+    INPUTS: None
+
+    RETURN CODE: None
+
+*******************************************************************************/
+static void register_led(void)
+{
+    const esp_console_cmd_t cmd = {
+        .command = "led",
+        .help = "query and set LEDs",
+        .hint = NULL,
+        .func = &command_led,
+        .argtable = &led_args,
+    };
+    led_args.set =   arg_lit0("s", "set",   "set flag");
+    led_args.layer = arg_int0("l", "layer", "<0..7>", "layer");
+    led_args.x =     arg_int0("x", NULL,    "<0..3>", "x");
+    led_args.y =     arg_int0("y", NULL,    "<0..3>", "y");
+    led_args.red =   arg_int0("r", "red",   "<0..15>", "red value");
+    led_args.green = arg_int0("g", "green", "<0..15>", "green value");
+    led_args.blue =  arg_int0("b", "blue",  "<0..15>", "blue value");
+    led_args.end = arg_end(2);
+    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
 }
 
--- main/version.h
+++ main/version.h
@@ -4,7 +4,7 @@
 
 #define MAJOR 0
 #define MINOR 0
-#define BUILD 2
+#define BUILD 3
 
 #define CONFIG_FIRMWARE_STATUS_URL      "http://www.2dkits.com/kits/kit25/current_firmware.txt"
 #define CONFIG_TEST_FIRMWARE_STATUS_URL "http://www.2dkits.com/kits/kit25/current_test_firmware.txt"
